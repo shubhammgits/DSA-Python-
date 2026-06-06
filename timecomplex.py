d2e75e0cb9b6c403ee4c26fc82741d6ffb04863b
shubhammgits <shubhamm18.work@gmail.com>
2026-06-06 18:04:08 +0530

    readme

--- timecomplex.py
+++
@@ -1,9 +0,0 @@
-def intTostr(i):
-    if i==0:
-        return '0'
-    digits = '0123456789'
-    result = ''
-    while i>0:
-        result = digits[i%10] + result
-        i = i//10
-    return result
